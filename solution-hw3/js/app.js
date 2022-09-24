518430408d08dae3f045cb5b8d689a076a9906e9
parvathyanand1997 <parvatha@cs.cmu.edu>
2022-09-24 09:06:11 -0400

    dynamically populating the dropdown menu

---
+++ solution-hw3/js/app.js
@@ -0,0 +1,84 @@
+class glazes {
+
+    constructor(glazeType,glazePrice) 
+    {
+        this.glazeType = glazeType;
+        this.glazePrice = glazePrice;
+	}
+}
+
+const keepOriginal = new glazes('Keep original',0.0);
+const sugarMilk = new glazes('Sugar milk',0.0);
+const vanillaMilk = new glazes('Vanilla milk',0.50);
+const doubleChocolate = new glazes('doubleChocolate',1.50);
+
+
+class packs {
+
+    constructor(packType,packPrice) 
+    {
+        this.packType = packType;
+        this.packPrice = packPrice;
+		
+	}
+}
+
+const one = new packs(1,1);
+const three = new packs(3,3);
+const six = new packs(6,5);
+const twelve = new packs(12,10);
+
+
+let glazeObjectList = [keepOriginal,sugarMilk,vanillaMilk,doubleChocolate];
+let packObjectList = [one,three,six,twelve];
+
+
+
+
+
+let glazeMenu = document.getElementById("glazeMenu"); 
+for (let i = 0; i < glazeObjectList.length; i++) 
+{
+    console.log("its working");
+    let option1 = document.createElement("option");
+    option1.textContent = glazeObjectList[i].glazeType;
+    glazeMenu.appendChild(option1);
+    console.log(option1);
+
+}
+
+
+   
+
+
+
+
+    
+ let packMenu= document.getElementById("packMenu"); 
+ for (let j = 0; j < packObjectList.length; j++) 
+    {
+        let option2 = document.createElement("option");
+        option2.textContent = packObjectList[j].packType;
+        packMenu.appendChild(option2);
+        console.log(option2);
+    }
+    
+    
+ 
+
+
+
+const basePrice = 2.49;
+let gPrice = 0.0;
+let pSize = 1;
+let totalPrice = 0.0;
+
+function priceCalculator(element) {
+    let choice = document.getElementById("x");
+
+
+
+
+}
+
+
