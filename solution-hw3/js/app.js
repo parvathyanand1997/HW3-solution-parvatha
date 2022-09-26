81b361761b62a306310f44629fcaaf49624e6dd6
parvathyanand1997 <parvatha@cs.cmu.edu>
2022-09-26 10:30:25 -0400

    formatting

--- solution-hw3/js/app.js
+++ solution-hw3/js/app.js
@@ -1,11 +1,15 @@
 class glazes {
 
+    glazeType;
+    glazePrice
     constructor(glazeType,glazePrice) 
     {
         this.glazeType = glazeType;
         this.glazePrice = glazePrice;
 	}
 }
+
+//initializing objects of glazes class with the type of glaze and its price
 
 const keepOriginal = new glazes('Keep original',0.0);
 const sugarMilk = new glazes('Sugar milk',0.0);
@@ -15,6 +19,9 @@ const doubleChocolate = new glazes('doubleChocolate',1.50);
 
 class packs {
 
+    packType;
+    packPrice;
+
     constructor(packType,packPrice) 
     {
         this.packType = packType;
@@ -23,16 +30,20 @@ class packs {
 	}
 }
 
+// initializing objects of packs class with the pack sizes and the price of each pack
 const one = new packs(1,1);
 const three = new packs(3,3);
 const six = new packs(6,5);
 const twelve = new packs(12,10);
 
 
+//Array of objects
+
 let glazeObjectList = [keepOriginal,sugarMilk,vanillaMilk,doubleChocolate];
 let packObjectList = [one,three,six,twelve];
 
 
+//Dynamically populating the dropdown menu
 
 let glazeMenu = document.getElementById("glazeMenu"); 
 for (let i = 0; i < glazeObjectList.length; i++) 
@@ -56,12 +67,13 @@ for (let j = 0; j < packObjectList.length; j++)
     }
      
 
+
 const basePrice = 2.49;
 let gPrice = 0.0;
 let pSize = 1;
 let totalPrice = 0.0;
 
-
+ //Function to extract the price of the selected glazing
 function glazingChange(element)
 {
 
@@ -75,12 +87,12 @@ function glazingChange(element)
             gPrice = Number(glazeObjectList[i].glazePrice);
         }
     }
-    console.log(gPrice);
+    //console.log(gPrice);
     calculatePrice(gPrice,pSize);
 
 }
 
-
+//Function to extract the price of the pack
 function packChange(element)
 {
     let selectedPackOption = element.value;
@@ -93,11 +105,11 @@ function packChange(element)
             pSize = Number(packObjectList[i].packPrice);
         }
     }
-    console.log(pSize);
+    //console.log(pSize);
     calculatePrice(gPrice,pSize);
 }
 
-
+//Function to calculate the price and display it
 function calculatePrice(gPrice,pSize)
 {
     totalPrice = (basePrice + gPrice)*pSize;
